fbb0ab17b5523adf34e1a0b7801f08fec0b4d02e
selectako <84815566+selectako@users.noreply.github.com>
2025-12-03 14:22:20 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,13 +2,13 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_YQmd7Nfal', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '10iadcbddpfqa2eko6qcmpcakg', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        clientSecret: '1iikta0jt0g4i1i8f830quqpj9leuo8oikus2c6di123kfmf6od7'
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
